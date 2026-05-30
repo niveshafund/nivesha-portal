eb739421567c3e2b36072cb185393f2eea582ebf
Mohan Verma <mohan@niveshaventures.com>
2026-05-30 12:29:33 -0700

    fix: supabase/ssr

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,5 +3,13 @@ import { createBrowserClient } from '@supabase/ssr';
 
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    cookieOptions: {
+      name: 'sb',
+      sameSite: 'lax',
+      secure: true,
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    },
+  }
 );
